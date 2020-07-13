556f0f3b30360694a3b722ec13cb1453984117c9
poojagaikwad1358 <poojagaikwad1358@gmail.com>
2020-07-13 18:36:57 +0530

    calculate total working hrs or days readched for month

--- empWageComputation.java
+++ empWageComputation.java
@@ -3,6 +3,7 @@ public class employeeWage {
        	public static final int IsPartTime=2;
         public static final int empRatePerHrs=20;
 	public static final int NumberofWorkingDays=20;
+	public static final int MaxHrsInMonth=100; //Usecase6 - total working hrs or days reached for month
 
 	public static void main(String[] args) {
 
@@ -10,10 +11,11 @@ public class employeeWage {
 	//UseCase1 - Check employee present or absent
 	int empHrs=0;
 	int empwage=0;
-	int TotalEmpWage=0;
-
-	for (int day=0; day<NumberofWorkingDays; day++)
+	int TotalWorkingDays=0;
+	int TotalEmpHrs=0;
+	while(TotalEmpHrs <= MaxHrsInMonth && TotalWorkingDays < NumberofWorkingDays)
 	{
+		TotalWorkingDays++;
 		int empcheck=((int)Math.floor(Math.random() * 10))%3;
 		switch ((empcheck)) //UseCase4- Switch case
 		{
@@ -26,10 +28,13 @@ public class employeeWage {
 			default:
 				empHrs=0;
 		}
-		empwage=empHrs*empRatePerHrs;
-		System.out.println("Employee Daily Wage= " +empwage); //Usecase2-daily wage
-		TotalEmpWage+=empwage; //UseCase5- calculate wages for month
+		TotalEmpHrs+=empHrs;
+		System.out.println("days: "+TotalWorkingDays + " Employee hrs: "+empHrs);
+		//empwage=empHrs*empRatePerHrs;
+		//System.out.println("Employee Daily Wage= " +empwage); //Usecase2-daily wage
+		//TotalEmpWage+=empwage; //UseCase5- calculate wages for month
 	}
+		int TotalEmpWage=TotalEmpHrs *empRatePerHrs;
 		System.out.println("Total employee wage: "+TotalEmpWage);
 }
 }
