d4291f11b3f3116a5d3e052a0d7f4d7069dc8489
poojagaikwad1358 <poojagaikwad1358@gmail.com>
2020-07-13 17:47:05 +0530

    Calculate wages for month

--- empWageComputation.java
+++ empWageComputation.java
@@ -2,6 +2,7 @@ public class employeeWage {
 	public static final int IsPresent=1;
        	public static final int IsPartTime=2;
         public static final int empRatePerHrs=20;
+	public static final int NumberofWorkingDays=20;
 
 	public static void main(String[] args) {
 
@@ -9,20 +10,26 @@ public class employeeWage {
 	//UseCase1 - Check employee present or absent
 	int empHrs=0;
 	int empwage=0;
+	int TotalEmpWage=0;
 
-	int empcheck=((int)Math.floor(Math.random() * 10))%3;
-	switch ((empcheck)) //UseCase4- Switch case
+	for (int day=0; day<NumberofWorkingDays; day++)
 	{
-		case IsPresent:
-			empHrs=8;
-			break;
-		case IsPartTime:  //Usecase3-part time
-			empHrs=4;
-			break;
-		default:
-			empHrs=0;
-	}
+		int empcheck=((int)Math.floor(Math.random() * 10))%3;
+		switch ((empcheck)) //UseCase4- Switch case
+		{
+			case IsPresent:
+				empHrs=8;
+				break;
+			case IsPartTime:  //Usecase3-part time
+				empHrs=4;
+				break;
+			default:
+				empHrs=0;
+		}
 		empwage=empHrs*empRatePerHrs;
 		System.out.println("Employee Daily Wage= " +empwage); //Usecase2-daily wage
+		TotalEmpWage+=empwage; //UseCase5- calculate wages for month
 	}
+		System.out.println("Total employee wage: "+TotalEmpWage);
+}
 }
