31af1d7f753220b311ca600e677902743c42e475
poojagaikwad1358 <poojagaikwad1358@gmail.com>
2020-07-13 16:48:02 +0530

    Added switch case statement

--- empWageComputation.java
+++ empWageComputation.java
@@ -1,19 +1,27 @@
 public class employeeWage {
-     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage computation Problem");
+	public static final int IsPresent=1;
+       	public static final int IsPartTime=2;
+        public static final int empRatePerHrs=20;
+
+	public static void main(String[] args) {
+
+	System.out.println("Welcome to Employee Wage computation Problem");
 	//UseCase1 - Check employee present or absent
-	int IsPresent=1;
-	int IsPartTime=2;
-	int empRatePerHrs=20;
 	int empHrs=0;
 	int empwage=0;
 
-	double empcheck=Math.floor(Math.random() * 10)%3;
-	if ( empcheck == IsPresent)
-		empHrs=8;
-	else if (empcheck == IsPartTime) //UseCase3- Part time
-		empHrs=4;
-
+	int empcheck=((int)Math.floor(Math.random() * 10))%3;
+	switch ((empcheck)) //UseCase4- Switch case
+	{
+		case IsPresent:
+			empHrs=8;
+			break;
+		case IsPartTime:  //Usecase3-part time
+			empHrs=4;
+			break;
+		default:
+			empHrs=0;
+	}
 		empwage=empHrs*empRatePerHrs;
 		System.out.println("Employee Daily Wage= " +empwage); //Usecase2-daily wage
 	}
