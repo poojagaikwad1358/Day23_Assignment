81ce44e393aeda55f49aeb3fedf32d5506886761
poojagaikwad1358 <poojagaikwad1358@gmail.com>
2020-07-13 15:56:08 +0530

    Added Part time employee wage

--- empWageComputation.java
+++ empWageComputation.java
@@ -3,15 +3,16 @@ public class employeeWage {
         System.out.println("Welcome to Employee Wage computation Problem");
 	//UseCase1 - Check employee present or absent
 	int IsPresent=1;
+	int IsPartTime=2;
 	int empRatePerHrs=20;
 	int empHrs=0;
 	int empwage=0;
 
-	double empcheck=Math.floor(Math.random() * 10)%2;
+	double empcheck=Math.floor(Math.random() * 10)%3;
 	if ( empcheck == IsPresent)
 		empHrs=8;
-	else
-		empHrs=0;
+	else if (empcheck == IsPartTime) //UseCase3- Part time
+		empHrs=4;
 
 		empwage=empHrs*empRatePerHrs;
 		System.out.println("Employee Daily Wage= " +empwage); //Usecase2-daily wage
